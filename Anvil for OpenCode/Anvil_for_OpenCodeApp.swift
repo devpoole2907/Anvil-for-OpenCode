20ca9768e20e5a0cc014d7c4d6a2293b8af68d33
James Poole <jamesdpoole@icloud.com>
2026-05-02 12:23:56 +1200

    Initial Commit

---
+++ Anvil for OpenCode/Anvil_for_OpenCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Anvil_for_OpenCodeApp.swift
+//  Anvil for OpenCode
+//
+//  Created by James Poole on 02/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct Anvil_for_OpenCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
